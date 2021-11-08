d7774aa2a502f49f9369361210d9c4b3fdc85a8f
Jacky <bestfish16@gmail.com>
2021-11-08 14:26:10 +0800

    feat: icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+svg(@click="handleClick")
+  use(:xlink:href="`#${name}`")
+</template>
+
+<script>
+export default {
+  name: "Icon",
+  props: {
+    name: String
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    handleClick() {
+      this.$emit("click")
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped></style>
