6afda07ceb00a3b91717faaa14b3a1ba3849523c
Jacky <bestfish16@gmail.com>
2021-11-11 11:08:34 +0800

    feat: sub form 切版

--- src/views/Activity/PopularActivity.vue
+++ src/views/Activity/PopularActivity.vue
@@ -1,18 +1,45 @@
 <template lang="pug">
 .hot-container
+  .form
+    .form-wrapper
+      .input-bar
+        .input
+          input(type="text", v-model="keyword", placeholder="搜尋關鍵字")
+      .select-bar
+        .select
+          select(v-model="type")
+            option(value="", disabled) 類別
+            option(value="view") 景點
+            option(value="activity") 活動
+        .select
+          select(v-model="county")
+            option(value="", disabled) 不分縣市
+            option(value="view") 景點
+            option(value="view") 美食
+        .search
+          Icon(name="search")
   PopularCity
 </template>
 
 <script>
 import PopularCity from "@/components/PopularCity.vue"
+import Icon from "@/components/Icon"
+import "@/assets/svg/search.svg"
 export default {
   name: "PopularActivity",
   components: {
-    PopularCity
+    Form: () => import("@/components/Form"),
+    PopularCity,
+    Icon
   },
   data() {
-    return {}
-  }
+    return {
+      type: "",
+      county: "",
+      keyword: ""
+    }
+  },
+  methods: {}
 }
 </script>
 
@@ -22,4 +49,32 @@ export default {
   padding: 43px 16px 40px
   margin: 0 auto
   max-width: 1280px
+  .form
+    display: none
++rwdMin(768px)
+  .hot-container
+    padding: 41px 65px 55px
+    .form
+      display: block
+      max-width: 430px
+      margin: 0 auto
+      margin-bottom: 38px
+      .form-wrapper
+        .select-bar
+          margin-top: 9px
+          display: flex
+          gap: 8px
+          .select
+            height: 40px
+            flex-grow: 1
+          .search
+            width: 40px
+            height: 40px
+            background-color: $primary
+            border-radius: 6px
+            box-shadow: 0px 4px 3px rgba(13, 11, 12, 0.2)
++rwdMin(1280px)
+  .hot-container
+    .form
+      display: none
 </style>
