496d8537cf2b3508f1898d6209c45adf7ef935e5
Jacky <bestfish16@gmail.com>
2021-11-12 15:13:39 +0800

    feat: footer 製作

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
 .app
-  //- Header
   router-view
+  Footer
 </template>
 
 <script>
-// import Header from "@/components/Header"
+import Footer from "@/components/Footer"
 export default {
   components: {
-    // Header
+    Footer
   },
   data() {
     return {}
---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+.footer
+  | Taiwan Tourguide © Code: Jacky / Design: KT
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  components: {},
+  data() {
+    return {}
+  },
+  computed: {}
+}
+</script>
+
+<style lang="sass" scoped>
+@import "@/assets/sass/index.sass"
+.footer
+  padding: 9px
+  width: 100%
+  background-color: $white
+  text-align: center
+  color: $grey-002
+  font-size: 12px
+  line-height: 14px
+</style>
