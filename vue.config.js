5562109234e3a52771f87fd6b3cf984e10bde385
Jacky <bestfish16@gmail.com>
2022-04-07 22:26:05 +0800

    feat: 增加public path

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,8 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/taiwan-travel-guide" : "",
   productionSourceMap: false,
   filenameHashing: true,
   configureWebpack: {
